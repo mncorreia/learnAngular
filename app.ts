db873c03c1918568a8cab2ec894c445d6cfc014b
matheus <matheus@localhost.localdomain>
2018-01-30 23:27:13 -0200

    Adição de arquivos inicias do projeto

---
+++ app.ts
@@ -0,0 +1,6 @@
+let message:string = 'Help me Obi-wan';
+console.log(message);
+
+let episode:number = 4
+episode += episode + 1;
+console.log(episode);
---
+++ dist/app.js
@@ -0,0 +1,5 @@
+var message = 'Help me Obi-wan';
+console.log(message);
+var episode = 4;
+episode += episode + 1;
+console.log(episode);
